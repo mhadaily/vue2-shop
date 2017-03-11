03d5e3c7dfd72ef89c4404ba4fa37a05be9f4ccc
Majid <mhadaily@gmail.com>
2017-03-11 23:18:38 +0800

    import accounting and pluralize

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
+import accounting from 'accounting';
+import pluralize from 'pluralize';
 
 import store from './store';
 import App from './App';
 import router from './router';
 
 Vue.use(ElementUI);
+Vue.filter('pluralize', pluralize);
+Vue.filter('formatMoney', accounting.formatMoney);
 
 Vue.config.productionTip = false;
 
