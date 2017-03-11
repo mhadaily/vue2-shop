94b1224f3ec14feea3724e7e7586ca4d19e4858d
Majid <mhadaily@gmail.com>
2017-03-11 14:16:33 +0800

    Slideshow component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <el-row>
+      <el-col :span="24">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
   </div>
-
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  export default {
+    name: 'app',
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,39 +1,46 @@
 <template>
   <div class="hello">
+    <Slideshow></Slideshow>
     <h1>{{ msg }}</h1>
-    <router-link to="/products"><el-button>Go to Products</el-button></router-link>
-
+    <router-link to="/products">
+      <el-button>Go to Products</el-button>
+    </router-link>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js Shop App By Majid Hajian',
-    };
-  },
-};
+  import Slideshow from './homepage/Slideshow';
+
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js Shop App By Majid Hajian',
+      };
+    },
+    components: {
+      Slideshow,
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
