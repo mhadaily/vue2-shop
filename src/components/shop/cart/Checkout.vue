b61659569389ba4b79de3664c8b542e9d9d9c96f
Majid <mhadaily@gmail.com>
2017-03-11 23:45:35 +0800

    design checkout page

--- src/components/shop/cart/Checkout.vue
+++ src/components/shop/cart/Checkout.vue
@@ -1,29 +1,45 @@
 <template>
   <div class="checkout">
-    <p>
-      <button :disabled="!products.length" @click="checkout(products)">Checkout</button>
-    </p>
+    <el-steps :space="100" :active="3" finish-status="success">
+      <el-step title="Select"></el-step>
+      <el-step title="Cart"></el-step>
+      <el-step title="Checkout"></el-step>
+      <el-step title="Payment"></el-step>
+      <el-step title="Done"></el-step>
+    </el-steps>
+    <br>
+
+    <Items
+      v-for="product in products"
+      :product="product"
+      :key="product.id"></Items>
+
+    <Summery :total="total"></Summery>
+
+
+    <br>
     <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
     <router-link to="/thanks">
-      <el-button>Go to Thanks</el-button>
+      <el-button :disabled="!products.length" @click="checkout(products)" type="danger">PAYMENT and DONE</el-button>
     </router-link>
   </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex';
+  import Items from './../cart/Items';
+  import Summery from './../cart/Summery';
 
   export default {
-    name: 'checkout',
-    data() {
-      return {
-        msg: 'Checkout Page',
-      };
-    },
     computed: {
       ...mapGetters({
+        products: 'cartProducts',
         checkoutStatus: 'checkoutStatus',
       }),
+    },
+    components: {
+      Items,
+      Summery,
     },
     methods: {
       checkout(products) {
