90271871d62e01c03038e86f39118330c396035b
Majid <mhadaily@gmail.com>
2017-03-12 00:28:28 +0800

    add container to checkout

--- src/components/shop/cart/Checkout.vue
+++ src/components/shop/cart/Checkout.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="checkout">
-    <el-steps :space="100" :active="3" finish-status="success">
-      <el-step title="Select"></el-step>
-      <el-step title="Cart"></el-step>
-      <el-step title="Checkout"></el-step>
-      <el-step title="Payment"></el-step>
-      <el-step title="Done"></el-step>
-    </el-steps>
-    <br>
+    <div class="page-container">
+      <el-steps :space="100" :active="3" finish-status="success">
+        <el-step title="Select"></el-step>
+        <el-step title="Cart"></el-step>
+        <el-step title="Checkout"></el-step>
+        <el-step title="Payment"></el-step>
+        <el-step title="Done"></el-step>
+      </el-steps>
+      <br>
 
-    <Items
-      v-for="product in products"
-      :product="product"
-      :key="product.id"></Items>
+      <Items
+        v-for="product in products"
+        :product="product"
+        :key="product.id"></Items>
 
-    <Summery :total="total"></Summery>
+      <Summery :total="total"></Summery>
 
-
-    <br>
-    <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
-    <router-link to="/thanks">
-      <el-button :disabled="!products.length" @click="checkout(products)" type="danger">PAYMENT and DONE</el-button>
-    </router-link>
+      <br>
+      <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
+      <router-link to="/thanks">
+        <el-button :disabled="!products.length" @click="checkout(products)" type="danger">PAYMENT and DONE</el-button>
+      </router-link>
+    </div>
   </div>
 </template>
 
